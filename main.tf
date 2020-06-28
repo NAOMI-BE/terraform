9b307936dcaf2301a45e62f4c7b3ee64bd3381db
Naomi Owens <naomi.owens@bearingpoint.com>
2020-06-28 22:13:51 +0100

    adding main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = "eu-west-1"
+}
+resource "aws_instance" "example" {
+    ami = "ami-0f38cdec7da648424"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "naomi-terraform-example"
+    }
+}
